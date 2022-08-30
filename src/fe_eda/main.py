e1ec8a5dae74363cb2c7e559f8adf120f18d6a69
Adam Carpenter <adamc@jlab.org>
2022-08-30 16:04:30 -0400

    Redirects STDOUT to a report file if plots are saved

--- src/fe_eda/main.py
+++ src/fe_eda/main.py
@@ -1,13 +1,17 @@
 import argparse
 import math
 import os
+import sys
 from datetime import datetime
 
 from matplotlib import pyplot as plt
+import pandas as pd
 import utils
 from config import cfg
 
 plt.style.use('seaborn-whitegrid')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
 
 
 def main():
@@ -31,6 +35,11 @@ def main():
         out_dir = None
     cfg['out_dir'] = out_dir
 
+    if cfg['out_dir'] is None:
+        cfg['report_file'] = None
+    else:
+        cfg['report_file'] = f"{cfg['out_dir']}/{cfg['operation']}.txt"
+
     if not os.path.isfile(args.file):
         print(f"Error: input file not found '{args.file}'")
         exit(1)
@@ -45,31 +54,48 @@ def main():
 
     # Do the requested operation
     if cfg['operation'] == 'summary':
-        do_summary(cfg['file'])
+        # Redirect standard out to report file if one is given
+        if cfg['report_file'] is not None:
+            with open(cfg['report_file'], "w") as sys.stdout:
+                do_summary(cfg['file'])
+        else:
+            do_summary(cfg['file'])
 
 
 def do_summary(file: str) -> None:
 
+    print("\n\n##########################")
+    print("Loading data")
+    print("##########################")
     df = utils.load_csv(file, det_lb=-math.inf, det_ub=math.inf)
-    print("Processing data")
 
     # Get data in useful subsets
+    print("\n\n##########################")
+    print("Processing data")
+    print("##########################")
     gmes_cols, gamma_cols, neutron_cols, date_cols = utils.get_cols(df, rad_suffix="_lag-1",)
     gmes_df, gamma_df, neutron_df, detector_df = utils.get_data_subsets(df, gmes_cols=gmes_cols, gamma_cols=gamma_cols,
                                                                         neutron_cols=neutron_cols, date_cols=date_cols)
 
     # Summarize the gradient and detector responses
-    print("Summarizing GMES and Detector in plot form")
-    #utils.summarize_gmes(gmes_df, date_cols, split_dates=True)
-
     if cfg['show_plots']:
         plt.ion()
 
+    print("\n\n##########################")
+    print("Summarizing GMES")
+    print("##########################")
     plt.figure(figsize=(20, 5))
     utils.summarize_gmes(gmes_df, date_cols, split_dates=False)
+
+    print("\n\n##########################")
+    print("Summarizing Gamma Radiation")
+    print("##########################")
     plt.figure()
     utils.summarize_detector(gamma_df, date_cols, title="Gamma Radiation rem/hr", filename="gamma_summary.png")
 
+    print("\n\n##########################")
+    print("Summarizing Neutron Radiation")
+    print("##########################")
     plt.figure()
     if cfg['show_plots']:
         plt.ioff()
@@ -78,7 +104,9 @@ def do_summary(file: str) -> None:
     return
 
     # Generate lots of plots showing correlation information
+    print("\n\n##########################")
     print("Summarizing correlations.")
+    print("##########################")
     utils.summarize_correlations(gmes_df, gamma_df, neutron_df, detector_df, date_cols)
 
 
